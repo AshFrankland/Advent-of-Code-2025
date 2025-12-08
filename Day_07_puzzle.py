86f87a5137321a94a7591bf0b41bf8a839e41430
Ash Frankland <afrankland@spartaglobal.com>
2025-12-08 04:41:08 +0000

    Day 7 Setup

---
+++ Day_07_puzzle.py
@@ -0,0 +1,17 @@
+from pprint import pprint
+
+def get_Input():
+    input = 'test_input.txt'
+    #input = 'Day_07_input.txt'
+    with open(input) as puzzle_Input:
+        txt = puzzle_Input.readlines()
+        raw_Input = []
+        for line in txt:
+            raw_Input.append(line.rstrip('\n'))
+        return raw_Input
+
+def main():
+    print(get_Input())
+
+if __name__ == '__main__':
+    main()
