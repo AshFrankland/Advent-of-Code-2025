73307c4afcfc89a1504bb1b679cadb83c404b79c
Ash Frankland <afrankland@spartaglobal.com>
2025-12-08 02:47:56 +0000

    Day 5 Solved

--- Day_05_puzzle.py
+++ Day_05_puzzle.py
@@ -1,17 +1,52 @@
-from pprint import pprint
-
 def get_Input():
-    input = 'test_input.txt'
-    #input = 'Day_05_input.txt'
+    input = 'Day_05_input.txt'
     with open(input) as puzzle_Input:
         txt = puzzle_Input.readlines()
         raw_Input = []
         for line in txt:
             raw_Input.append(line.rstrip('\n'))
-        return raw_Input
+        range_Strs = raw_Input[:raw_Input.index('')]
+        id_Strs = raw_Input[raw_Input.index('') + 1:]
+        ranges = []
+        for entry in range_Strs:
+            bound_Strs = entry.split('-')
+            bounds = (int(bound_Strs[0]), int(bound_Strs[1]))
+            ranges.append(bounds)
+        ids = []
+        for id in id_Strs:
+            ids.append(int(id))
+        return ranges, ids
+
+def find_Fresh(ranges, ids):
+    fresh_Count = 0
+    for id in ids:
+        for ran in ranges:
+            if id >= ran[0] and id <= ran[1]:
+                fresh_Count += 1
+                break
+    return fresh_Count
+
+def how_Fresh(ranges):
+    merged = []
+    while ranges:
+        new = []
+        check_Range = ranges.pop()
+        for index in range(len(merged)):
+            if max(check_Range[0], merged[index][0]) <= min(check_Range[1], merged[index][1]):
+                check_Range = (min(check_Range[0], merged[index][0]), max(check_Range[1], merged[index][1]))
+            else:
+                new.append(merged[index])
+        new.append(check_Range)
+        merged = new
+    fresh_Ids = 0
+    for ran in merged:
+        fresh_Ids += ran[1] - ran[0] + 1
+    return fresh_Ids
 
 def main():
-    print(get_Input())
+    ranges, ids = get_Input()
+    print(find_Fresh(ranges, ids))
+    print(how_Fresh(ranges))
 
 if __name__ == '__main__':
     main()
